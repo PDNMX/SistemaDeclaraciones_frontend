f5f055170d361ee33b013138bbccba7fe76b8af8
Norma Verónica Trinidad <nvth28@gmail.com>
2021-04-19 02:51:18 -0500

    chore: clean proxy target url

--- proxy.conf.js
+++ proxy.conf.js
@@ -10,7 +10,7 @@ const proxyConfig = [
   {
     context: '/api',
     pathRewrite: { '^/api': '' },
-    target: 'https://api.chucknorris.io',
+    target: '',
     changeOrigin: true,
     secure: false,
   },
